e0dffb89ac0dcdc75da56765dbe452242168968b
Doang <doangcespedesloreto@gmail.com>
2023-09-12 09:42:59 -0400

    creando y barajeando el deck

--- asets/js/juego.js
+++ asets/js/juego.js
@@ -1,1 +1,45 @@
-alert("habla claro")
+
+/**
+ * 2C = Two of Clubs
+ * 2D = Two of Diaminds
+ * 2H = Two of Hearts
+ * 2S = Two of Spades 
+ */
+
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K' ];
+
+const crearDeck = () => {
+    for( let i = 2; i<= 10; i++){
+        for( let tipo of tipos){
+            deck.push( i + tipo)
+        }
+    }
+
+    for( let tipo of tipos){
+        for(let esp of especiales){
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    console.log( deck );
+
+
+}
+
+crearDeck();
+
+const pedirCarta = () => {
+
+    if(deck.length === 0){ throw 'No hay cartas en el deck';} //El THROW nos ayuda a finalizar nuestra app y mandar un mensaje de error 
+    const carta = deck.pop()
+    console.log(deck);
+    console.log(carta);
+    return carta;
+
+}
+
+pedirCarta()
